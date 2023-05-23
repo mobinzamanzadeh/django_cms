213519db8d21e692ebd4bf096a21214d63f1be43
Mobin Zamanzadeh <mobin.zamanzadeh@gmail.com>
2023-05-23 17:02:44 +0330

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,75 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    ),
+    image = models.ImageField(upload_to='uploads/%y/%m/%d/'),
+    content = models.TextField()
+    tag = models.ManyToManyField(Tag)
+    category = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(
+        choices=((0, "Draft"), (1, "Publish"), (2, "Delete")),
+        default=0
+    )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.author) + ' comment ' + str(self.content)
+
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).reverse()
+
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
