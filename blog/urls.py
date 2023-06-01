c1368a97eaf251738241dc7a0219514c1f1d4702
Mobin Zamanzadeh <mobin.zamanzadeh@gmail.com>
2023-06-01 11:59:25 +0330

    add templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from blog.views import post_list, post_detail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('posts/', post_list, name='post_list'),
+    path('post/<slug:slug>/', post_detail, name='post_detail'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib.auth.models import User
+
+from blog.models import User, Post, Comment
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(status=1).order_by('-published_at')
+    return render(request, 'post-list.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=1)
+    comments = Comment.objects.filter(post=post)
+    return render(request, 'post-detail.html', {'post': post, 'comments': comments})
